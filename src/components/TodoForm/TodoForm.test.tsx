e105da2819d143ec3cf11db6d414c6618dab4bec
chapetta <cha.petta@hotmail.com>
2025-09-19 17:16:50 -0300

    testes: adicionando teste no TodoForm

--- src/components/TodoForm/TodoForm.test.tsx
+++ src/components/TodoForm/TodoForm.test.tsx
@@ -1,7 +1,31 @@
-import { describe, it, expect } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import TodoForm from ".";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
-describe('Placeholder', () => {
-  it('should pass as placeholder', () => {
-    expect(true).toBe(true);
+describe('TodoForm', () => {
+  const addTodoMock = vi.fn()
+  
+  it('Testa se o input para adicionar as tarefas está na tela', async () => {
+    render(<TodoForm addTodo={addTodoMock} />)
+
+    const input = screen.getByPlaceholderText('Create a new todo...')
+      
+
+      expect(input).toBeInTheDocument()
   });
+
+  it('Testa se da para digitar no input e adicionar a tarefa', async () => {
+    const user = userEvent.setup()
+    render(<TodoForm addTodo={addTodoMock} />)
+
+    const input = screen.getByPlaceholderText('Create a new todo...')
+    await user.type(input, 'Nova tarefa')
+    await user.keyboard('{Enter}')
+
+    expect(input).toHaveValue('Nova tarefa')
+    expect(addTodoMock).toHaveBeenCalledTimes(1)
+
+
+  })
 });
